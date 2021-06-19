098adf793309ef7c7522db1f6f4c79f994647c59
Jake <jake.a.reddy@gmail.com>
2021-06-19 09:47:09 +0300

    moved nav to own component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
 import Recommended from './components/Recommended'
+import Navigation from './components/Navigation'
 
 const App = () => {
   const [page, setPage] = useState('authors')
@@ -33,21 +34,8 @@ const App = () => {
 
   return (
     <div>
-      <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        {token !== null && (
-          <>
-            <button onClick={() => setPage('add')}>add book</button>
-            <button onClick={() => setPage('recommend')}>recommended</button>
-            <button onClick={() => handleLogout()}>Logout</button>
-          </>
-        )}
-        {token === null && (
-          <button onClick={() => setPage('login')}>login</button>
-        )}
-      </div>
-      <div>
+      <Navigation token={token} setPage={setPage} handleLogout={handleLogout} />
+      <div className='page-wrapper'>
         {page === 'recommend' ? (
           <Recommended show={page === 'recommend'} />
         ) : null}
